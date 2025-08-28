262282d5a282cea48a3ad44e92acf0eaf038c923
Egunza-dev <egunza.dev@gmail.com>
2025-08-28 15:28:14 +0300

    fix: correct google sign-in config schema

--- app.config.ts
+++ app.config.ts
@@ -9,7 +9,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
     ...config,
     android: {
       ...config.android,
-      config: {
+      
         googleSignIn: {
           // Your Firebase project's API key, loaded from your .env file
           apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -17,7 +17,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
           // The certificate hash from './gradlew signingReport' for your standalone build
           certificateHash: "50:C9:66:86:3A:38:6E:AD:E3:14:95:62:E6:22:4A:8A:BA:23:59:E9"
         }
-      }
+      
     }
   };
 };
