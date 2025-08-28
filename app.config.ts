ed8852ba80d53d6c54ff39892101a5e60fbb8d50
Egunza-dev <egunza.dev@gmail.com>
2025-08-28 17:08:47 +0300

    fix: configure google sign-in plugin in app.json

--- app.config.ts
+++ app.config.ts
@@ -8,16 +8,8 @@ export default ({ config }: ConfigContext): ExpoConfig => {
   return {
     ...config,
     android: {
-      ...config.android,
-      
-        googleSignIn: {
-          // Your Firebase project's API key, loaded from your .env file
-          apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-          
-          // The certificate hash from './gradlew signingReport' for your standalone build
-          certificateHash: "50:C9:66:86:3A:38:6E:AD:E3:14:95:62:E6:22:4A:8A:BA:23:59:E9"
-        }
-      
+      ...config.android     
+              
     }
   };
 };
